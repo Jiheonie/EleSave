60335aa4a82c4c1f237489146dd2211e701be2d5
Jiheonie <hoangtunguyen2002@gmail.com>
2023-05-12 15:23:05 +0700

    cover adafruit key

--- constances/adafruit.js
+++ constances/adafruit.js
@@ -1,7 +1,7 @@
 const AIO_account = {
   username: "nghiachutuan68",
-  // key: "<given key>",
-  key: "aio_fKuh44BFyQ2BZr8MuTgjPdCStt6S",
+  key: "<given key>",
+  // key: "aio_fKuh44BFyQ2BZr8MuTgjPdCStt6S",
 };
 
 export default AIO_account;
